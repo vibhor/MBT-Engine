806bed97b59e2ac641947e122ec6a50c5513caf6
Sharad Binjola <sharadbinjola626@gmail.com>
2010-01-30 14:21:59 +0530

    first commit

---
+++ src/Diagram.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Document
+ * Date: 30 Jan, 2010
+ * Time: 2:12:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Diagram {
+}
